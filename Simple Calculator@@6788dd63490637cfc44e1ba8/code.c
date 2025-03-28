2ed70af79db43cadd008780dbc9e78d2891bb233
Jayant Chopra <choprajayant2@gmail.com>
2025-03-29 04:08:04 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -2,32 +2,39 @@
 #include <stdio.h>
 int calc(int a, int b, char c)
 {
-    switch(c)
+    if (b==0 && c=='/')
     {
-        case '+':
-        printf("%d", a+b);
-        break;
-        case '-':
-        printf("%d", a-b);
-        break;
-        case '*':
-        printf("%d", a*b);
-        break;
-        case '/':
-        printf("%d", a/b);
-        break;
-        default:
-        printf("Error");
-        break;
+        printf("error");
     }
+    else
+    {
+        switch(c)
+        {
+            case '+':
+            printf("%d", a+b);
+            break;
+            case '-':
+            printf("%d", a-b);
+            break;
+            case '*':
+            printf("%d", a*b);
+            break;
+            case '/':
+            printf("%d", a/b);
+            break;
+            default:
+            printf("error");
+            break;
+        }
+    }
+    
 }
 
 int main()
 {
     int a,b;
     char c;
-    scanf("%d %d", &a,&b);
-    scanf("%c", &c);
+    scanf("%d %d %c", &a,&b,&c);
 
     calc(a,b,c);
 }
